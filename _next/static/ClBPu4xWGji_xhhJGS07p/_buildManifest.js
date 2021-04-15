4409e75d6df8412c8882be1b2fcf82e666a152ef
Rising Odegua <risingodegua@gmail.com>
2021-04-15 15:07:07 +0100

    Update actions script

--- _next/static/ClBPu4xWGji_xhhJGS07p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-afac079455ea673a4886.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-95dae70cdcef922e3fb6.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
